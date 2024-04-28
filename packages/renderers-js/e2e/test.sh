9d78443c9f44a594775b74852d080d998c97c8d7
Loris Leiva <loris.leiva@gmail.com>
2024-04-28 20:48:38 +0100

    Only start local validator if needed in e2e tests

--- packages/renderers-js/e2e/test.sh
+++ packages/renderers-js/e2e/test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+function start_validator() {
+    if ! lsof -t -i:8899; then
+        echo "Starting solana-test-validator"
+        solana-test-validator >/dev/null 2>&1 &
+    fi
+}
+
 function test_project() {
     ./e2e/generate.cjs $1
     cd e2e/$1
@@ -7,17 +14,6 @@ function test_project() {
     cd ../..
 }
 
-function start_validator() {
-    echo "Starting solana-test-validator"
-    solana-test-validator >/dev/null 2>&1 &
-}
-
-function stop_validator() {
-    echo "Stopping solana-test-validator"
-    pkill -f solana-test-validator
-}
-
 start_validator
 test_project system
 test_project memo
-stop_validator
