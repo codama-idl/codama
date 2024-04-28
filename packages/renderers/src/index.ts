7f549a84649eaa5ab06743b055c3af56deeaf303
Loris Leiva <loris.leiva@gmail.com>
2024-04-28 20:38:05 +0100

    Add renderers package (#14)

---
+++ packages/renderers/src/index.ts
@@ -0,0 +1,3 @@
+export { renderVisitor as renderJavaScriptVisitor } from '@kinobi-so/renderers-js';
+export { renderVisitor as renderJavaScriptUmiVisitor } from '@kinobi-so/renderers-js-umi';
+export { renderVisitor as renderRustVisitor } from '@kinobi-so/renderers-rust';
