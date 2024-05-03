649bb184caf44f34ff411a26f8ae4b86aa390682
Loris Leiva <loris.leiva@gmail.com>
2024-05-03 14:13:19 +0100

    Update test-unit.mjs

--- packages/internals/scripts/test-unit.mjs
+++ packages/internals/scripts/test-unit.mjs
@@ -7,16 +7,16 @@ const watch = argv.watch;
 
 // Prepare build and test arguments.
 const buildArgs = ['--config', `internals/tsup.config.tests.${platform}.ts`];
-const esmTestArgs = ['--config', `../../node_modules/@kinobi-so/internals/ava.config.${platform}-esm.mjs`];
+// const esmTestArgs = ['--config', `../../node_modules/@kinobi-so/internals/ava.config.${platform}-esm.mjs`];
 const cjsTestArgs = ['--config', `../../node_modules/@kinobi-so/internals/ava.config.${platform}-cjs.mjs`];
 
 if (watch) {
     // Build and run the tests concurrently in watch mode.
-    await $`concurrently "tsup ${buildArgs} --watch" "ava ${esmTestArgs} --watch" --hide 0 --prefix none --kill-others`;
+    await $`concurrently "tsup ${buildArgs} --watch" "ava ${cjsTestArgs} --watch" --hide 0 --prefix none --kill-others`;
 } else {
     // Build and run the tests.
     await $`tsup ${buildArgs}`;
-    await $`ava ${esmTestArgs}`;
+    // await $`ava ${esmTestArgs}`;
     if (platform !== 'react-native') {
         await $`ava ${cjsTestArgs}`;
     }
