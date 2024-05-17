eeb1438f0d1602f18b6751897782110eff511c0e
febo <febo@tensor.so>
2024-05-17 05:16:47 -0400

    Remove console log

--- packages/renderers-rust/test/errorsPage.test.ts
+++ packages/renderers-rust/test/errorsPage.test.ts
@@ -27,8 +27,6 @@ test('it renders codes for errors', () => {
     // When we render it.
     const renderMap = visit(node, getRenderMapVisitor());
 
-    console.log(renderMap.get('errors/spl_token.rs'));
-
     // Then we expect the following errors with codes.
     codeContains(renderMap.get('errors/spl_token.rs'), [`InvalidInstruction = 0x1770,`, `InvalidProgram = 0x1B58,`]);
 });
