306ce4058a8f71df0f114095d485c7998508e2c9
Loris Leiva <loris.leiva@gmail.com>
2024-08-09 15:32:19 +0100

    Update index.ts

--- packages/library/src/cli/index.ts
+++ packages/library/src/cli/index.ts
@@ -9,11 +9,11 @@ program.name('kinobi').description('Manage your Kinobi IDLs').version(version);
 program
     .command('run')
     .description('Runs a visitor on your Kinobi IDL')
-    .argument('<test>', 'test argument', (code?: number) => {
-        return code ?? 42;
-    })
-    .action((test: number, context) => {
-        console.log({ context, test });
+    .argument('<foo>', 'test required argument')
+    .argument('[bar]', 'test optional argument')
+    .option('-b, --baz <char>', 'test option')
+    .action((foo, bar, options) => {
+        console.log({ bar, foo, options });
     });
 
 export function run(argv: readonly string[]) {
