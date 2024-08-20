2c22ca48905dd839ad6e7325982dab817c33cc23
Loris Leiva <loris.leiva@gmail.com>
2024-08-20 22:25:34 +0100

    Fix TS bug on `accept` and `update` Kinobi function

--- packages/library/test/index.test.ts
+++ packages/library/test/index.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 
-import { identityVisitor, rootNode } from '../src';
+import { createFromRoot, identityVisitor, programNode, rootNode, rootNodeVisitor, voidVisitor } from '../src';
 
 test('it exports node helpers', () => {
     expect(typeof rootNode).toBe('function');
@@ -8,4 +8,18 @@ test('it exports node helpers', () => {
 
 test('it exports visitors', () => {
     expect(typeof identityVisitor).toBe('function');
+});
+
+test('it accepts visitors', () => {
+    const kinobi = createFromRoot(rootNode(programNode({ name: 'myProgram', publicKey: '1111' })));
+    const visitor = voidVisitor(['rootNode']);
+    const result = kinobi.accept(visitor) satisfies void;
+    expect(typeof result).toBe('undefined');
+});
+
+test('it updates the root node returned by visitors', () => {
+    const kinobi = createFromRoot(rootNode(programNode({ name: 'myProgram', publicKey: '1111' })));
+    const visitor = rootNodeVisitor(node => rootNode(programNode({ ...node.program, name: 'myTransformedProgram' })));
+    kinobi.update(visitor) satisfies void;
+    expect(kinobi.getRoot()).toEqual(rootNode(programNode({ name: 'myTransformedProgram', publicKey: '1111' })));
 });
