b2fafa7bc9612297f2be4109df93592e7c05a6ee
Loris Leiva <loris.leiva@gmail.com>
2024-10-15 11:16:32 +0100

    Add test for default struct traits

--- packages/renderers-rust/test/utils/traitOptions.test.ts
+++ packages/renderers-rust/test/utils/traitOptions.test.ts
@@ -1,4 +1,12 @@
-import { definedTypeNode, enumEmptyVariantTypeNode, enumTypeNode } from '@codama/nodes';
+import {
+    accountNode,
+    definedTypeNode,
+    enumEmptyVariantTypeNode,
+    enumTypeNode,
+    numberTypeNode,
+    structFieldTypeNode,
+    structTypeNode,
+} from '@codama/nodes';
 import { describe, expect, test } from 'vitest';
 
 import { getTraitsFromNode, TraitOptions } from '../../src/utils';
@@ -27,7 +35,34 @@ describe('default values', () => {
         ]);
     });
 
-    test.todo('it defaults to a set of traits for structs');
+    test('it defaults to a set of traits for structs', () => {
+        // Given an account node.
+        const node = accountNode({
+            data: structTypeNode([
+                structFieldTypeNode({ name: 'x', type: numberTypeNode('u64') }),
+                structFieldTypeNode({ name: 'y', type: numberTypeNode('u64') }),
+            ]),
+            name: 'Coordinates',
+        });
+
+        // When we get the traits from the node using the default options.
+        const { render, imports } = getTraitsFromNode(node);
+
+        // Then we expect the following traits to be rendered.
+        expect(render).toBe(
+            `#[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq, PartialEq)]\n` +
+                `#[cfg(feature = "serde", derive(Serialize, Deserialize))]`,
+        );
+
+        // And the following imports to be used.
+        expect([...imports.imports]).toStrictEqual([
+            'borsh::BorshSerialize',
+            'borsh::BorshDeserialize',
+            'serde::Serialize',
+            'serde::Deserialize',
+        ]);
+    });
+
     test.todo('it defaults to a set of traits for aliases');
     test.todo('it defaults to not using fully qualified names');
     test.todo('it defaults to a set of feature flags for traits');
