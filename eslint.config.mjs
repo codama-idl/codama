88bab9e6072c52c5334b76486082277b87cb46ec
Loris Leiva <loris.leiva@gmail.com>
2024-11-28 14:21:59 +0000

    Try disabling new rules again for CI
    Despite it working locally... :sob:

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,20 @@ export default tseslint.config([
     {
         files: ['**/*.ts'],
         extends: [solanaConfig],
+        rules: {
+            '@typescript-eslint/no-base-to-string': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/only-throw-error': 'off',
+            '@typescript-eslint/prefer-promise-reject-errors': 'off',
+            '@typescript-eslint/restrict-plus-operands': 'off',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/unbound-method': 'off',
+        },
     },
     {
         files: ['packages/nodes/**', 'packages/node-types/**'],
