bc2e306373838128c35950347fc18d8d265bb1e1
Loris Leiva <loris.leiva@gmail.com>
2025-01-31 17:49:06 +0000

    Update web3.js v2 URLs (#424)
    Update links to web3.js v2 so they point to the new repo.

--- packages/errors/src/stack-trace.ts
+++ packages/errors/src/stack-trace.ts
@@ -1,6 +1,6 @@
 /**
  * Heavily inspired by @solana/errors.
- * @see https://github.com/solana-labs/solana-web3.js/blob/master/packages/errors
+ * @see https://github.com/anza-xyz/solana-web3.js/blob/main/packages/errors
  */
 
 export function safeCaptureStackTrace(...args: Parameters<typeof Error.captureStackTrace>): void {
