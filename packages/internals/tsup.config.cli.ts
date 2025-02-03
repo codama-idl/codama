4ceeb5e6c479690fe878d25af6a5d48953adfa6a
Loris Leiva <loris.leiva@gmail.com>
2025-02-03 12:11:48 +0000

    Improve ESM and CJS support when importing modules in CLI (#430)
    * Better error handling
    
    * Better ESM and CJS support when importing modules
    
    * Add changeset

--- packages/internals/tsup.config.cli.ts
+++ packages/internals/tsup.config.cli.ts
@@ -4,10 +4,10 @@ import { getBuildConfig } from './tsup.config.base';
 
 export default defineConfig([
     {
-        ...getBuildConfig({ format: 'esm', platform: 'node' }),
+        ...getBuildConfig({ format: 'cjs', platform: 'node' }),
         entry: { cli: './src/cli/index.ts' },
         outExtension() {
-            return { js: `.mjs` };
+            return { js: `.cjs` };
         },
     },
 ]);
