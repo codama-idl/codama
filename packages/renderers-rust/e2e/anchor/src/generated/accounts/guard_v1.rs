2b45badbc800d862d478f19b8231daf2a88d6d56
Joe C <jcaulfield135@gmail.com>
2025-03-18 01:51:52 +0800

    Rust Renderer: `fetch` Fixes (#510)
    * renderer-rust: use full path qualifier for `Pubkey` on account fetchers
    
    * renderer-rust: remove unnecessary borrows
    
    * add changeset

--- packages/renderers-rust/e2e/anchor/src/generated/accounts/guard_v1.rs
+++ packages/renderers-rust/e2e/anchor/src/generated/accounts/guard_v1.rs
@@ -54,7 +54,7 @@ impl<'a> TryFrom<&solana_program::account_info::AccountInfo<'a>> for GuardV1 {
 #[cfg(feature = "fetch")]
 pub fn fetch_guard_v1(
     rpc: &solana_client::rpc_client::RpcClient,
-    address: &Pubkey,
+    address: &solana_program::pubkey::Pubkey,
 ) -> Result<crate::shared::DecodedAccount<GuardV1>, std::io::Error> {
     let accounts = fetch_all_guard_v1(rpc, &[*address])?;
     Ok(accounts[0].clone())
@@ -63,10 +63,10 @@ pub fn fetch_guard_v1(
 #[cfg(feature = "fetch")]
 pub fn fetch_all_guard_v1(
     rpc: &solana_client::rpc_client::RpcClient,
-    addresses: &[Pubkey],
+    addresses: &[solana_program::pubkey::Pubkey],
 ) -> Result<Vec<crate::shared::DecodedAccount<GuardV1>>, std::io::Error> {
     let accounts = rpc
-        .get_multiple_accounts(&addresses)
+        .get_multiple_accounts(addresses)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
     let mut decoded_accounts: Vec<crate::shared::DecodedAccount<GuardV1>> = Vec::new();
     for i in 0..addresses.len() {
@@ -88,7 +88,7 @@ pub fn fetch_all_guard_v1(
 #[cfg(feature = "fetch")]
 pub fn fetch_maybe_guard_v1(
     rpc: &solana_client::rpc_client::RpcClient,
-    address: &Pubkey,
+    address: &solana_program::pubkey::Pubkey,
 ) -> Result<crate::shared::MaybeAccount<GuardV1>, std::io::Error> {
     let accounts = fetch_all_maybe_guard_v1(rpc, &[*address])?;
     Ok(accounts[0].clone())
@@ -97,10 +97,10 @@ pub fn fetch_maybe_guard_v1(
 #[cfg(feature = "fetch")]
 pub fn fetch_all_maybe_guard_v1(
     rpc: &solana_client::rpc_client::RpcClient,
-    addresses: &[Pubkey],
+    addresses: &[solana_program::pubkey::Pubkey],
 ) -> Result<Vec<crate::shared::MaybeAccount<GuardV1>>, std::io::Error> {
     let accounts = rpc
-        .get_multiple_accounts(&addresses)
+        .get_multiple_accounts(addresses)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
     let mut decoded_accounts: Vec<crate::shared::MaybeAccount<GuardV1>> = Vec::new();
     for i in 0..addresses.len() {
