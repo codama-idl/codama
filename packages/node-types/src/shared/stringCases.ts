55122fda70f5a832fd791cda17172e41cbe55c1b
Loris Leiva <loris.leiva@gmail.com>
2025-09-16 13:21:25 +0100

    Implement branded types without unique symbol (#853)

--- packages/node-types/src/shared/stringCases.ts
+++ packages/node-types/src/shared/stringCases.ts
@@ -1,19 +1,19 @@
 export type TitleCaseString = string & {
-    readonly __stringCase: unique symbol;
+    readonly ['__stringCase:codama']: 'titleCase';
 };
 
 export type PascalCaseString = string & {
-    readonly __stringCase: unique symbol;
+    readonly ['__stringCase:codama']: 'pascalCase';
 };
 
 export type CamelCaseString = string & {
-    readonly __stringCase: unique symbol;
+    readonly ['__stringCase:codama']: 'camelCase';
 };
 
 export type KebabCaseString = string & {
-    readonly __stringCase: unique symbol;
+    readonly ['__stringCase:codama']: 'kebabCase';
 };
 
 export type SnakeCaseString = string & {
-    readonly __stringCase: unique symbol;
+    readonly ['__stringCase:codama']: 'snakeCase';
 };
