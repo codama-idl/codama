ef5d104af0746d46a41a2733f1cd5600c538aad9
Loris Leiva <loris.leiva@gmail.com>
2025-12-02 15:00:47 +0000

    Remove treeshake workaround for Rollup's deprecated assert syntax (#942)

--- tsup.config.base.ts
+++ tsup.config.base.ts
@@ -54,14 +54,6 @@ export function getBuildConfig(options: BuildOptions): TsupConfig {
         publicDir: true,
         pure: ['process'],
         sourcemap: format !== 'iife',
-
-        // Treeshaking already happens with esbuild but tsup offers this options
-        // for "better" treeshaking strategies using Rollup as an additional step.
-        // The issue is Rollup now uses the deprecated "assert" import keyword by default
-        // And tsup doesn't offer a way to change this behavior. Since the CLI package
-        // relies on using the "with" keyword to dynamically import JSON files,
-        // we can't use Rollup for treeshaking.
-        treeshake: false,
     };
 }
 
